3111d52d1b2a026235da3f995d87a3ee8583cd41
Nicolas Urdangarin <103833940+Nadrak1@users.noreply.github.com>
2024-06-28 16:19:46 -0300

    agregando cambios metodo backTracking

--- src/tpe/utils/Backtracking_Greedy.java
+++ src/tpe/utils/Backtracking_Greedy.java
@@ -9,83 +9,61 @@ import tpe.Tarea;
 public class Backtracking_Greedy {
     private int mejorTiempoDeEjecucion;
     private LinkedList<Procesador> mejorSolucion;
-    private final int maximoDeCriticas;
     
     public Backtracking_Greedy() {
         this.mejorTiempoDeEjecucion = Integer.MAX_VALUE;
         this.mejorSolucion = new LinkedList<Procesador>();
-        this.maximoDeCriticas = 2;
     }
 
     // CONSIDERANDO EN TODO MOMENTO QUE UNA TAREA ES INDIVISIBLE, 
     // ES DECIR, NO PUEDE SER ASIGNADA A VARIOS PROCESADORES
     
-    public LinkedList<Procesador> back( LinkedList<Tarea> tareas, 
-    									LinkedList<Procesador> procesadores,
-    									int tiempoMaxProcNoRefrigerado) {
+    public LinkedList<Procesador> back(LinkedList<Tarea> tareas, LinkedList<Procesador> procesadores,int tiempoMaxProcNoRefrigerado) {
         int tiempoActual = 0;
-        // un contador de tareasAsignadas que si no es 0 quiere decir que quedan tareas por asignar
-        int tareasAsignadas = tareas.size(); 
+        int tareasAsignadas = tareas.size(); // un contador de tareasAsignadas que si no es 0 quiere decir que quedan tareas por asignar
         
-        back(tareas, procesadores, tiempoActual, tareasAsignadas, 
-        	 tiempoMaxProcNoRefrigerado);
-
+        back(tareas, procesadores, tiempoActual, tareasAsignadas, tiempoMaxProcNoRefrigerado);
         if(mejorSolucion.isEmpty()) {
-        	System.out.println("Sucedio un error en la carga de tareas a procesadores!!!!");
+        	System.out.println("Mas tareas criticas de lo que pueden soportar los procesadores!!!!!");
         }
+        System.out.println("el mejor tiempo es "+this.mejorTiempoDeEjecucion);
         return mejorSolucion;
     }
 
-    private void back(LinkedList<Tarea> tareas, 
-    				  LinkedList<Procesador> procesadores, int tiempoActual, 
-    				  int tareasAsignadas, int tiempoMaxProcNoRefrigerado) {
-        if (tareasAsignadas == 0) { 								// cuando no quedan mas tareas que asignar
-            if (tiempoActual <= this.mejorTiempoDeEjecucion) {      // y el tiempo de la llamda actual es menor que el guardado anteriormente ( mejorTiempoDeEjecucion se inicializa en Integer.MAX_VALUE
-                this.mejorTiempoDeEjecucion = tiempoActual;			//mejor tiempo ahora es el de la llamda actual
-                actualizarMejorSolucion(procesadores);				//guardo esos procesadores con esas tareas
+    private void back(LinkedList<Tarea> tareas, LinkedList<Procesador> procesadores, int tiempoActual, int tareasAsignadas, int tiempoMaxProcNoRefrigerado) {
+        if (tareasAsignadas == 0) {
+        	System.out.println(procesadores);
+        	if (tiempoActual <= this.mejorTiempoDeEjecucion) {
+                this.mejorTiempoDeEjecucion = tiempoActual;
+                actualizarMejorSolucion(procesadores); 
             }
         } else {
-            Tarea tareaActual = tareas.get(tareasAsignadas - 1);     //la tarea arranca en el ultimo indice 
+            Tarea tareaActual = tareas.get(tareasAsignadas - 1);
 
-            for (int i = 0; i < procesadores.size(); i++) {			
+            for (int i = 0; i < procesadores.size(); i++) {
                 Procesador procesador = procesadores.get(i);
-
-                if ((!procesador.getTareas().contains(tareaActual) ) && 		//si el procesador no tiene guardada esa tarea 
-                	 (procesador.getTareasCriticas() < maximoDeCriticas) ) {					// y no tiene mas de 2 tareas criticas (utilizo 2 ya que es un valor constante y no se aclara que pueda cambiar )		
-                	if(procesador.isEstaRefrigerado()) {				//esto es en caso de q este refrigerado
-	                    procesador.agregarTarea(tareaActual);												//agrego la tarea al procesador
-	                    int nuevoTiempoProcesador = procesador.getTiempoDeEjecucionAcumulado();				//guardo el tiempo de ejecucion acumulado de mi procesador actual
-	                    // tiempoActual cambiar nombre !!
-	                    int tiempoMaximoActual = Math.max(tiempoActual, nuevoTiempoProcesador); 		//comparo el tiempo que tengo acumulado de mi procesador con mas tiempo anterior a esta ejecucion
-	                     																				//con el procesador de la ejecucion actual , y guarda el mayor de los dos
-	                    tareasAsignadas--;											
-	                    if (tiempoMaximoActual <= this.mejorTiempoDeEjecucion) {			//mi poda ya que si el tiempo mayor acumulado es mayor a mi mejor tiempo esta rama no me va a dar una mejor solucion!
-	                        back(tareas, procesadores, tiempoMaximoActual, 
-	                        	 tareasAsignadas, tiempoMaxProcNoRefrigerado);
+                
+                if (!procesador.getTareas().contains(tareaActual) &&
+                    (!tareaActual.isEsCritica() || procesador.getTareasCriticas() < 2) &&
+                    (procesador.isEstaRefrigerado() || (procesador.getTiempoDeEjecucionAcumulado() + tareaActual.getTiempoEjecucion()) <= tiempoMaxProcNoRefrigerado)) {
+                	
+	                    procesador.agregarTarea(tareaActual);
+	                    int nuevoTiempoProcesador = procesador.getTiempoDeEjecucionAcumulado();
+	                    int tiempoMaximoActual = Math.max(tiempoActual, nuevoTiempoProcesador); // tiempoActual cambiar nombre !!
+	                    
+	                    tareasAsignadas--;
+	                    if (tiempoMaximoActual <= this.mejorTiempoDeEjecucion) {
+	                        back(tareas, procesadores, tiempoMaximoActual, tareasAsignadas, tiempoMaxProcNoRefrigerado);
 	                    }
 
-	                    procesador.eliminarTarea(tareaActual); 								//deshago los cambios para poder continuar con la recursion
+	                    procesador.eliminarTarea(tareaActual);
 	                    tareasAsignadas++;
-                		}else if( ( procesador.getTiempoDeEjecucionAcumulado() + 
-                					tareaActual.getTiempoEjecucion()  ) <= tiempoMaxProcNoRefrigerado) {		//esto es en caso de que NO este refrigerado y el tiempo actual mas la siguiente tarea sea menor a
-		                    procesador.agregarTarea(tareaActual);												//al tiempo maximo de refrigeracion
-		                    int nuevoTiempoProcesador = procesador.getTiempoDeEjecucionAcumulado();
-		                    int tiempoMaximoActual = Math.max(tiempoActual, nuevoTiempoProcesador);
-		                  
-		                    tareasAsignadas--;
-		                    if (tiempoMaximoActual <= this.mejorTiempoDeEjecucion) {
-		                        back(tareas, procesadores, tiempoMaximoActual, 
-		                        	 tareasAsignadas, tiempoMaxProcNoRefrigerado);
-		                    }
-		                    procesador.eliminarTarea(tareaActual);
-		                    tareasAsignadas++;
-                		
-                	}
                 }
+                
             }
         }
     }
-
+     
     private void actualizarMejorSolucion(LinkedList<Procesador> procesadores) {
         mejorSolucion.clear();
         for (Procesador p : procesadores) {
